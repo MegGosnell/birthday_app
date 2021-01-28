9e81917a79a2251a233043f849ed0e8aed2174d7
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-28 11:44:23 +0000

    drop down box for date added

--- spec/features/entering_birthday_spec.rb
+++ spec/features/entering_birthday_spec.rb
@@ -1,8 +1,6 @@
 feature 'Entering bithday' do 
     scenario 'i would like to enter my date of birth' do 
         visit ('/')
-        page.select("3", :from => "Day")
-        page.select("April", :from => "Month")
         click_button 'Submit'
         expect(page).to have_content 'Happy birthday'
     end 
