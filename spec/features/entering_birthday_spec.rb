d94af4bdbf780781d62205d46daf2c8792b35e2b
megan gosnell <megan_gosnell@aol.co.uk>
2021-01-28 11:54:38 +0000

    refractoring

--- spec/features/entering_birthday_spec.rb
+++ spec/features/entering_birthday_spec.rb
@@ -1,4 +1,12 @@
+
 feature 'Entering bithday' do 
+    scenario 'the user enters their name so they can see happy birthday' do 
+        visit ('/')
+        fill_in 'name', with: 'Meg'
+    end 
+    
+    
+
     scenario 'i would like to enter my date of birth' do 
         visit ('/')
         click_button 'Submit'
